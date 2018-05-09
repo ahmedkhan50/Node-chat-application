4711b25f204ca2209bdeb9ef56203a335d0192f1
Nabeel Ahmed Khan <nakhan@tracfone.com>
2018-05-09 00:02:13 -0400

    layout changes for chat application and styling

--- public/js/index.js
+++ public/js/index.js
@@ -14,39 +14,44 @@ socket.on('newMessage', function (newmessage) {
     $("#messages").append(li);
 });
 
-socket.on('newLocationMessage',function(newLocation){
+socket.on('newLocationMessage', function (newLocation) {
     var li = $("<li></li>");
     var a = $("<a target='blank'>My current location</a>");
     li.text(`${newLocation.from}:`);
-    a.attr('href',newLocation.url);
+    a.attr('href', newLocation.url);
     li.append(a);
     $("#messages").append(li);
 });
 
 $("#message-form").on("submit", function (event) {
     event.preventDefault();
-
+    var messageTextBox = $(".chat__footer #message-form input[name='message']");
     socket.emit('createMessage', {
         from: 'user',
-        text: $("#message-form input[name='message']").val()
+        text: messageTextBox.val()
     }, function (message) {
         console.log('Got it..' + message);
+        messageTextBox.val('');
     });
 
 });
 
 var locationButton = $("#send-location");
-locationButton.on('click',function(event){
-    if(!navigator.geolocation){
+locationButton.on('click', function (event) {
+    if (!navigator.geolocation) {
         return alert('geolocation not supported by this browser!');
     }
 
-    navigator.geolocation.getCurrentPosition(function(position){
-      socket.emit('createLocationMessage',{
-          latitude:position.coords.latitude,
-          longitude:position.coords.longitude
-      });
-    },function(){
-         alert('unable to fetch location!');
+    // disable button when fetching location...
+    locationButton.attr('disabled', 'disabled').text('Sending location..');
+    navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send location');
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+    }, function () {
+        locationButton.removeAttr('disabled').text('Send location');
+        alert('unable to fetch location!');
     });
 });
