fd9bf732022f5de5b4855e80d071a30f73dfd7e6
Nabeel Ahmed Khan <nakhan@tracfone.com>
2018-05-08 22:29:49 -0400

    added form for messages and show incoming messages

--- public/js/index.js
+++ public/js/index.js
@@ -8,6 +8,21 @@ socket.on('disconnect', function () {
     console.log('server disconnected');
 });
 
-socket.on('newMessage',function(newmessage){
-    console.log(newmessage); 
+socket.on('newMessage', function (newmessage) {
+    console.log(newmessage);
+    var li = $("<li></li>");
+    li.text(`${newmessage.from}:${newmessage.text}`);
+    $("#messages").append(li);
+});
+
+$("#message-form").on("submit", function (event) {
+    event.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'user',
+        text: $("#message-form input[name='message']").val()
+    }, function (message) {
+        console.log('Got it..' + message);
+    });
+
 });
--- server/server.js
+++ server/server.js
@@ -21,8 +21,9 @@ io.on('connection', (socket) => {
     // socket.broadcast.emit will send to all users except the one who opened application.
     socket.broadcast.emit('newMessage',generateMessage('admin','New User Joined!!'));
 
-    socket.on('createMessage', (message) => {
+    socket.on('createMessage', (message,callback) => {
         io.emit('newMessage',generateMessage(message.from,message.text));     
+        callback('this is from the server!!');
     });
 
     socket.on('disconnect', () => {
